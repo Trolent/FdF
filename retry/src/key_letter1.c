6c883158c3cb96865f16b28f6ec434b6ad126159
Timothée Rolland <rolland.timothee@gmail.com>
2024-02-14 11:29:05 +0100

    Auto-updating from Home

---
+++ retry/src/key_letter1.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   key_letter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trolland <trolland@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/13 15:22:27 by trolland          #+#    #+#             */
+/*   Updated: 2024/02/14 11:28:31 by trolland         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/fdf.h"
+
+void	key_right(t_vars *vars)
+{
+		vars->map->midy -= 1 * vars->map->zoom / 4 + 1;
+}
+
+void	key_left(t_vars *vars)
+{
+		vars->map->midy += 1 * vars->map->zoom / 4 + 1;
+}
+
+void	key_down(t_vars *vars)
+{
+		vars->map->midx -= 1 * vars->map->zoom / 4 + 1;
+}
+
+void	key_up(t_vars *vars)
+{
+		vars->map->midx += 1 * vars->map->zoom / 4 + 1;
+}
