9373044cc3e8ac2fefb8b84c8575047345d1e46a
Timothée Rolland <rolland.timothee@gmail.com>
2024-02-14 15:01:02 +0100

    Auto-updating from Home

--- retry/src/graphics.c
+++ retry/src/graphics.c
@@ -248,7 +248,7 @@ int	define_alt_color(t_map *map)
 						map->z_max - map->z_min, map->coord[i][j].z
 						- map->z_min);
 			else if (map->coord[i][j].z < 0 )
-				map->coord[i][j].alt_color = gradient(map->z_color, BLUE && 0xFF,
+				map->coord[i][j].alt_color = gradient(map->z_color, BLUE ^ 0xFF,
 						map->z_max - map->z_min, map->coord[i][j].z
 						- map->z_min);
 			j++;
